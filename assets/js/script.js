63682a0703ea80250e3b4a03e21e0ca3e8995ecf
Gamze Şirin <gamze1sirin@gmail.com>
2024-05-20 13:29:18 +0300

    sss bitti mobil bitti masaüstü bitti.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const faqItems = document.querySelectorAll('.faq-item')
 
-	// Her bir FAQ öğesi için event listener ekle
 	faqItems.forEach((item) => {
 		const question = item.querySelector('.faq-question')
 		const answer = item.querySelector('.faq-answer')
@@ -9,8 +8,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 		question.addEventListener('click', () => {
 			const isActive = item.classList.contains('active')
-
-			// Diğer tüm cevapları kapat ve iconları resetle
 			faqItems.forEach((otherItem) => {
 				if (otherItem !== item) {
 					otherItem.classList.remove('active')
@@ -20,15 +17,15 @@ document.addEventListener('DOMContentLoaded', function () {
 			})
 
 			if (!isActive) {
-				// Tıklanan öğeyi aktifleştir
 				item.classList.add('active')
 				answer.style.maxHeight = `${answer.scrollHeight}px`
 				icon.textContent = '-'
+				question.style.backgroundColor = ' #dfdbdb87'
 			} else {
-				// Tıklanan öğeyi deaktifleştir
 				item.classList.remove('active')
 				answer.style.maxHeight = '0'
 				icon.textContent = '+'
+				question.style.backgroundColor = ' #fff'
 			}
 		})
 	})
